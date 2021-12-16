18e41869a102a6e342019aaa348e2cce95a609dc
khoadang1404 <khoadang.1404@gmail.com>
2021-12-16 09:44:20 +0700

    up source

--- app/src/main/java/com/example/btck_qldoanphi/LoginForm.java
+++ app/src/main/java/com/example/btck_qldoanphi/LoginForm.java
@@ -2,7 +2,15 @@ package com.example.btck_qldoanphi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginForm extends AppCompatActivity {
 
@@ -10,5 +18,22 @@ public class LoginForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+
+        MaterialButton btnLogin = (MaterialButton) findViewById(R.id.btnLogin);
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText password = (EditText) findViewById(R.id.password);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("admin") && password.getText().toString().equals("123")){
+                    Toast.makeText(LoginForm.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginForm.this, MainActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginForm.this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/btck_qldoanphi/waiting_form.java
+++ app/src/main/java/com/example/btck_qldoanphi/waiting_form.java
@@ -2,13 +2,24 @@ package com.example.btck_qldoanphi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class waiting_form extends AppCompatActivity {
-
+    Handler wtf = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.waiting_form);
+        wtf.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(waiting_form.this, LoginForm.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
     }
 }
